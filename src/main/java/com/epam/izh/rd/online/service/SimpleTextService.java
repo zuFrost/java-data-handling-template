757457b0ab3b4838def61127e5b50f4e9c767c54
Alex Frost <zufrost.a@gmail.com>
2020-04-24 12:28:27 +0300

    make removeString method

--- src/main/java/com/epam/izh/rd/online/service/SimpleTextService.java
+++ src/main/java/com/epam/izh/rd/online/service/SimpleTextService.java
@@ -4,21 +4,21 @@ public class SimpleTextService implements TextService {
 
     /**
      * Реализовать функционал удаления строки из другой строки.
-     *
+     * <p>
      * Например для базовой строки "Hello, hello, hello, how low?" и строки для удаления ", he"
      * метод вернет "Hellollollo, how low?"
      *
-     * @param base - базовая строка с текстом
+     * @param base   - базовая строка с текстом
      * @param remove - строка которую необходимо удалить
      */
     @Override
     public String removeString(String base, String remove) {
-        return null; //TODO
+        return base.replaceAll(remove, ""); //TODO
     }
 
     /**
      * Реализовать функционал проверки на то, что строка заканчивается знаком вопроса.
-     *
+     * <p>
      * Например для строки "Hello, hello, hello, how low?" метод вернет true
      * Например для строки "Hello, hello, hello!" метод вернет false
      */
@@ -33,7 +33,7 @@ public class SimpleTextService implements TextService {
 
     /**
      * Реализовать функционал соединения переданных строк.
-     *
+     * <p>
      * Например для параметров {"Smells", " ", "Like", " ", "Teen", " ", "Spirit"}
      * метод вернет "Smells Like Teen Spirit"
      */
@@ -45,7 +45,7 @@ public class SimpleTextService implements TextService {
     /**
      * Реализовать функционал изменения регистра в вид лесенки.
      * Возвращаемый текст должен начинаться с прописного регистра.
-     *
+     * <p>
      * Например для строки "Load Up On Guns And Bring Your Friends"
      * метод вернет "lOaD Up oN GuNs aNd bRiNg yOuR FrIeNdS".
      */
@@ -69,18 +69,18 @@ public class SimpleTextService implements TextService {
         }
 
         //преобразовать массив символов в строку и вернуть в результат
-        return new  String(chars); //TODO
+        return new String(chars); //TODO
     }
 
     /**
      * Метод определяет, является ли строка палиндромом.
-     *
+     * <p>
      * Палиндром - строка, которая одинаково читается слева направо и справа налево.
-     *
+     * <p>
      * Например для строки "а роза упала на лапу Азора" вернется true, а для "я не палиндром" false
      */
     @Override
     public boolean isPalindrome(String string) {
-       return false; //TODO
+        return false; //TODO
     }
 }
