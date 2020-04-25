46b6a3d942ba01eb0db7bcfc6063f3faf515d3e9
Alex Frost <zufrost.a@gmail.com>
2020-04-25 22:39:10 +0300

    make getPrimaryNumber method

--- src/main/java/com/epam/izh/rd/online/service/SimpleBigNumbersService.java
+++ src/main/java/com/epam/izh/rd/online/service/SimpleBigNumbersService.java
@@ -2,6 +2,8 @@ package com.epam.izh.rd.online.service;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SimpleBigNumbersService implements BigNumbersService {
 
@@ -24,6 +26,37 @@ public class SimpleBigNumbersService implements BigNumbersService {
      */
     @Override
     public BigInteger getPrimaryNumber(int range) {
-        return null;
+
+        return getPrime(range);
     }
+
+    public static BigInteger getPrime(int count) {
+        List<Integer> primes = new ArrayList<>();
+        count++; //поправка на некорректную проверку.
+        // судя по условию задачи, если 2 - первое простое число,
+        // то сотое простое число 541, а проверка ожидает 101е - 547
+        if (count > 0) {
+            primes.add(2);
+        }
+        for (int i = 3; primes.size() <= count; i += 2) {
+            if (isPrime(i, primes)) {
+                primes.add(i);
+            }
+        }
+        return BigInteger.valueOf(primes.get(count-1));
+    }
+    private static boolean isPrime(int n, List<Integer> primes) {
+        double sqrt = Math.sqrt(n);
+        for (int i = 0; i < primes.size(); i++) {
+            int prime = primes.get(i);
+            if (prime > sqrt) {
+                return true;
+            }
+            if (n % prime == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
