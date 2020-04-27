ccaeada83ab43043c77f69e90c6aa6c77c4bf9ca
Alex Frost <zufrost.a@gmail.com>
2020-04-27 14:35:01 +0300

    make maskSensitiveData method

--- src/main/java/com/epam/izh/rd/online/service/SimpleRegExpService.java
+++ src/main/java/com/epam/izh/rd/online/service/SimpleRegExpService.java
@@ -1,5 +1,9 @@
 package com.epam.izh.rd.online.service;
 
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SimpleRegExpService implements RegExpService {
 
     /**
@@ -11,6 +15,18 @@ public class SimpleRegExpService implements RegExpService {
      */
     @Override
     public String maskSensitiveData() {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(new File("C:\\Java\\Java Course\\Java. Fundamentals\\java-data-handling-template\\src\\main\\resources\\sensitive_data.txt"))));
+            String line = reader.readLine();
+            Pattern pattern  = Pattern.compile("(?<=\\d{4}\\s)((\\d{4}\\s\\d{4}))(?=\\s\\d{4})");
+            Matcher matcher = pattern.matcher(line);
+            return matcher.replaceAll("**** ****");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 
